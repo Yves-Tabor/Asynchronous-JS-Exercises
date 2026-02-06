dd9b5e795ddc95fcbab2f1f6b6676a4415a1b33d
Yves-Tabor <walvesoutis.gmail.com>
2026-02-06 23:33:25 +0200

    ..

--- FetchWithPromiseAll.js
+++ FetchWithPromiseAll.js
@@ -1,19 +1,25 @@
+// Fetch form these url, associate each username with a key todo of his id
+
 // const users = "https://jsonplaceholder.typicode.com/users";
 // const todos = "https://jsonplaceholder.typicode.com/todos";
 
-// async function fetchUserTodos(url1,url2){
-//     const [res1, res2] = await Promise.all([fetch(url1), fetch(url2)]).then(response=> response);
-
-// const [usersRes, postRes] = await Promise.all([
-//     res1.json(),
-//     res2.json()
-//     ])
-// const userWithTodos = usersRes.map((user)=>({
-//     ...user,
-//     todos: postRes.filter((todo) => todo.userId === user.id)
+// async function fetchUsersWithTodos(url1, url2){
+//     try{
+//         const usersResponse = await fetch(url1);
+//         const todosResponse = await fetch(url2);
+//         const users = await usersResponse.json();
+//         const todos = await todosResponse.json();
+        
+//         const object = users.map(user=>{
+//             return {
+//                 ...user,
+//                 todos: todos.filter(todo=> todo.userId === user.id)
+//             }
+//         })
+//         console.log(object);
+//     }catch(error){
+//         console.log("An error occured: ",error)
+//     }
     
-// })
-// )
-// console.log("userWithTodos: ", userWithTodos);
 // }
-// fetchUserTodos(users, todos); 
+// fetchUsersWithTodos(users, todos);
